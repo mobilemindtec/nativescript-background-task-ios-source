519e14a397773f0e8aab147d1eafc34802a74da8
Ricardo Bocchi <ricardobocchi@gmail.com>
2016-06-07 18:37:06 -0300

    copy files bug fix

--- NSBackgroundTaskCopyFiles.m
+++ NSBackgroundTaskCopyFiles.m
@@ -17,6 +17,19 @@
 
 			NSLog(@"move file %@ to %@", _fromFile, _toFile);
 
+		    if([fileManager fileExistsAtPath: _toFile ] == YES){
+		    	NSLog(@"deleting destination file %@ before download..", _toFile);
+		    	NSError *deleteError;
+		    	[fileManager removeItemAtPath: _toFile error:&deleteError];
+
+				if(deleteError){
+					NSLog(@"error on delete file to %@ -> %@", _toFile, deleteError);
+					[self.delegate onError: [deleteError description]];
+					return;
+				}
+
+		    }
+
 			[fileManager moveItemAtPath: _fromFile toPath: _toFile error: &moveError];
 
 			if(moveError)
